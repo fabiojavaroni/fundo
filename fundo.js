7f33e16e12601b9cfc3c0978704f836d8c02124b
fabiojavaroni <54987728+fabiojavaroni@users.noreply.github.com>
2019-09-13 14:59:14 -0300

    Add files via upload

---
+++ fundo.js
@@ -0,0 +1,57 @@
+function espacial1(){
+  background(bg);      
+}
+
+function carregarespacial1(){
+  bg = loadImage('https://raw.githubusercontent.com/fabiojavaroni/fundo/master/espa%C3%A7o-1.png');      
+}
+
+
+
+
+function espacial2(){
+  background(bg2);      
+}
+
+function carregarespacial2(){
+  bg2 = loadImage('https://raw.githubusercontent.com/fabiojavaroni/fundo/master/espa%C3%A7o-2.png');      
+}
+
+
+function espacial3(){
+  background(bg3);      
+}
+
+function carregarespacial3(){
+  bg3 = loadImage('https://raw.githubusercontent.com/fabiojavaroni/fundo/master/espa%C3%A7o-3.png');      
+}
+
+
+
+function espacial4(){
+  background(bg4);      
+}
+
+function carregarespacial4(){
+  bg4 = loadImage('https://raw.githubusercontent.com/fabiojavaroni/fundo/master/espa%C3%A7o-4.png');      
+}
+
+
+
+function espacial5(){
+  background(bg5);      
+}
+
+function carregarespacial5(){
+  bg5 = loadImage('https://raw.githubusercontent.com/fabiojavaroni/fundo/master/espa%C3%A7o-5.png');      
+}
+
+
+function espacial6(){
+  background(bg6);      
+}
+
+function carregarespacial5(){
+  bg6 = loadImage('https://raw.githubusercontent.com/fabiojavaroni/fundo/master/espa%C3%A7o-6.png');      
+}
+
